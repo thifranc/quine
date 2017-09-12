5129c7983bb61b73f64cd163bed2e1069bbe4991
Thifranc <thifranc@student.42.fr>
2017-09-12 20:39:12 +0200

    tested ok

--- Sully.c
+++ Sully.c
@@ -1,15 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+extern const char *__progname;
 int main(){
 int i = 5;
+int next;
+if (strcmp(__progname,"Sully") == 0)
+next = i;
+else
+next = i - 1;
 if (i >= 0){
 char title[100];
-snprintf(title, 100, "Sully_%d.c", i);
- char *s= "#include <stdio.h>%c#include <stdlib.h>%cint main(){%cint i = %d;%cif (i > 0){%cchar title[100];%csnprintf(title, 100, %cSully_%%d.c%c, i);%c char *s= %c%s%c;%cFILE *fd = fopen(title, %cw%c);%cfprintf(fd, s, 10, 10, 10, i - 1, 10, 10, 10, 34, 34, 10, 34, s, 34, 10, 34, 34, 10, 10, 34, 34, 10, 10, 10, 10);%cfclose(fd);snprintf(title, 100, %cgcc -o Sully_%%d Sully_%%d.c && ./Sully_%%d%c, i, i, i);%csystem(title);%c}%creturn 1;%c}";
- FILE *fd = fopen(title, "w");
-fprintf(fd, s, 10, 10, 10, i - 1, 10, 10, 10, 34, 34, 10, 34, s, 34, 10, 34, 34, 10, 10, 34, 34, 10, 10, 10, 10);
+char *s="#include <stdio.h>%c#include <stdlib.h>%c#include <string.h>%cextern const char *__progname;%cint main(){%cint next;%cint i = %d;%cif (strcmp(__progname,%cSully%c) == 0)%cnext = i;%celse%cnext = i - 1;%cif (i >= 0){%cchar title[100];%cchar *s=%c%s%c;%csnprintf(title, 100, %cSully_%%d.c%c, next);%cFILE *fd = fopen(title, %cw%c);%cfprintf(fd, s, 10, 10, 10, 10, 10, 10, next, 10, 34, 34, 10, 10, 10, 10, 10, 10, 34, s, 34, 10, 34, 34, 10, 34, 34, 10, 10, 34, 34, 10, 10, 10, 10, 10, 10);%csnprintf(title, 100, %cgcc -o Sully_%%d Sully_%%d.c && ./Sully_%%d%c, next, next, next);%cfclose(fd);%csystem(title);%c}%creturn 1;%c}%c";
+snprintf(title, 100, "Sully_%d.c", next);
+FILE *fd = fopen(title, "w");
+fprintf(fd, s, 10, 10, 10, 10, 10, 10, next, 10, 34, 34, 10, 10, 10, 10, 10, 10, 34, s, 34, 10, 34, 34, 10, 34, 34, 10, 10, 34, 34, 10, 10, 10, 10, 10, 10);
+snprintf(title, 100, "gcc -o Sully_%d Sully_%d.c && ./Sully_%d", next, next, next);
 fclose(fd);
-snprintf(title, 100, "gcc -o Sully_%d Sully_%d.c && ./Sully_%d", i, i, i);
 system(title);
 }
 return 1;
