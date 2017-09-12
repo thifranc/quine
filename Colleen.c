5ffd27f1fe6aee9eec08e78872d4eefa1c14b3ac
thifranc <thifranc@student.42.fr>
2017-09-12 09:34:47 +0200

    tested ok

--- Colleen.c
+++ Colleen.c
@@ -4,14 +4,14 @@
  */
 void print()
 {
-	char *s="#include<stdio.h>\n/*\n * yo\n */\nvoid print()\n{\n	char *s=\"\%s\";\n	printf(\"\%s\", s);\n}\nint main()\n{\n/*\n * ok\n */\n	print();	return 0;\n}";
-	printf("#include<stdio.h>\n/*\n * yo\n */\nvoid print()\n{\n	char *s=\"%s\";\n	printf(\"%%s\", s);\n}\nint main()\n{\n/*\n * ok\n */\n	print();	return 0;\n}", s);
+char *s="#include<stdio.h>%c/*%c * yo%c */%cvoid print()%c{%cchar *s=%c%s%c;%cprintf(s, 10, 10, 10, 10, 10, 10, 34, s, 34, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10);%c}%cint main()%c{%c/*%c * ok%c */%cprint();%creturn 0;%c}%c";
+printf(s, 10, 10, 10, 10, 10, 10, 34, s, 34, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10);
 }
 int main()
 {
 /*
  * ok
  */
-	print();
-	return 0;
+print();
+return 0;
 }
